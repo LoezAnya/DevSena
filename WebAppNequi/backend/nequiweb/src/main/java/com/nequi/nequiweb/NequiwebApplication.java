55fc32035194a3beb7da941a1790ab1ec9bb47d7
Loez Anya <loez.anya@gmail.com>
2025-10-25 16:03:42 -0500

    Restart

---
+++ WebAppNequi/backend/nequiweb/src/main/java/com/nequi/nequiweb/NequiwebApplication.java
@@ -0,0 +1,13 @@
+package com.nequi.nequiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NequiwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NequiwebApplication.class, args);
+	}
+
+}
